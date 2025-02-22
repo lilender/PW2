21ef087f4e6ad03f1a804e553f7f55e9388c98e1
Ritzel2086070 <100053852+Ritzel2086070@users.noreply.github.com>
2025-02-21 19:42:27 -0600

    Welcome :v

---
+++ front-react/src/components/TopFanfics.tsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+function TopFanfics(){
+    return (
+        <div className='row tops'>
+            <div className='col-3'>
+
+            </div>
+            <div className='col-9 text-container'>
+                <h2 className='title'>Mirrors</h2>
+                <h3 className='author'>By Lilender</h3>
+                <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel dolor eros. Aenean sed mi nisi. Aliquam eu nisl eget libero lobortis posuere. Praesent semper, urna tristique porttitor tincidunt, velit enim vehicula nulla, vitae gravida ipsum nulla ut risus. Nam et turpis iaculis, congue eros tincidunt, fermentum elit.</p>
+            </div>
+        </div>
+    );
+}
+
+export default TopFanfics;
